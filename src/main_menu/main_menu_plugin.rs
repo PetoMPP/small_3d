c132e320e27b455ecee67cb7bfea7c13a738c036
PetoMPP <piotreq22@gmail.com>
2024-05-12 13:52:09 +0200

    implement pause ui with bevy nodes and vector shapes

--- src/main_menu/main_menu_plugin.rs
+++ src/main_menu/main_menu_plugin.rs
@@ -1,85 +1,104 @@
+use crate::common::plugins::ui_plugin::{styles, UiOnClick, UiOnClickBundle};
+use crate::game::game_plugin::GameState;
 use crate::resources::game_assets::GameAssets;
 use crate::resources::loadable::Loadable;
 use crate::resources::text_styles::{FontSize, FontType};
 use crate::{AppState, TextStyles};
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts};
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, ui_system.run_if(in_state(AppState::MainMenu)));
+        app.add_systems(OnEnter(AppState::MainMenu), init_main_menu)
+            .add_systems(Update, (update_text).run_if(in_state(AppState::MainMenu)))
+            .add_systems(OnExit(AppState::MainMenu), cleanup_main_menu);
     }
 }
 
-fn ui_system(
-    mut commands: Commands,
-    mut egui_contexts: EguiContexts,
+fn update_text(
+    mut query: Query<(&mut Text, &MenuText)>,
     asset_server: Res<AssetServer>,
     game_assets: Res<GameAssets>,
     text_styles: Res<TextStyles>,
 ) {
-    let font_name = egui::FontFamily::Name(FontType::Bold.as_ref().into());
-    // we need fonts to draw ui
-    if !egui_contexts
-        .ctx_mut()
-        .fonts(|f| f.families().contains(&font_name))
-    {
+    let Some((mut text, _)) = query.iter_mut().next() else {
+        return;
+    };
+
+    if text.sections[0].value.as_str() == READY_TEXT {
         return;
     }
-    egui::CentralPanel::default().show(egui_contexts.ctx_mut(), |ui| {
-        ui.with_layout(
-            egui::Layout {
-                main_align: egui::Align::Center,
-                main_justify: true,
-                cross_align: egui::Align::Center,
-                cross_justify: true,
-                ..Default::default()
-            },
-            |ui| {
-                render_main_menu(
-                    ui,
-                    &mut commands,
-                    game_assets.loaded(&asset_server) && text_styles.loaded(&asset_server),
-                    font_name,
-                )
-            },
-        );
-    });
+
+    if !game_assets.loaded(&asset_server) || !text_styles.loaded(&asset_server) {
+        return;
+    }
+
+    text.sections[0].value = READY_TEXT.to_string();
 }
 
-const READY_TEXT: &str = "Press to start";
+#[derive(Component)]
+struct MenuCamera;
+
+#[derive(Component)]
+struct MenuNode;
+
+#[derive(Component)]
+struct MenuText;
+
+const READY_TEXT: &str = "Press any key to start";
 const LOADING_TEXT: &str = "Loading..";
 
-fn render_main_menu(
-    ui: &mut egui::Ui,
-    commands: &mut Commands,
-    loaded: bool,
-    font_name: egui::FontFamily,
-) {
-    let text = match loaded {
-        true => READY_TEXT,
-        false => LOADING_TEXT,
-    };
-    if loaded
-        && ui
-            .add(
-                egui::Label::new(
-                    egui::RichText::new(text)
-                        .size(*FontSize::Large)
-                        .family(font_name)
-                        .color(egui::Color32::LIGHT_GRAY),
-                )
-                .selectable(false)
-                .sense(egui::Sense::click()),
-            )
-            .clicked()
-    {
-        commands.add(|world: &mut World| {
-            world
-                .resource_mut::<NextState<AppState>>()
-                .set(AppState::InGame);
-        })
+fn init_main_menu(mut commands: Commands, text_styles: Res<TextStyles>) {
+    commands.spawn((Camera2dBundle::default(), MenuCamera));
+    commands
+        .spawn((
+            NodeBundle {
+                style: styles::container(Val::Percent(100.0), Val::Percent(100.0)),
+                background_color: Color::rgb_from_array([0.2, 0.2, 0.2]).into(),
+                ..default()
+            },
+            MenuNode,
+            UiOnClickBundle {
+                ui_on_click: UiOnClick(set_in_game),
+                ..Default::default()
+            },
+        ))
+        .with_children(|root| {
+            root.spawn((
+                TextBundle {
+                    text: Text::from_section(
+                        LOADING_TEXT,
+                        text_styles.get(FontType::Bold, FontSize::Large, Color::WHITE),
+                    ),
+                    ..Default::default()
+                },
+                MenuText,
+            ));
+        });
+}
+
+fn set_in_game(world: &mut World) {
+    let asset_server = world.get_resource::<AssetServer>().unwrap();
+    let game_assets = world.get_resource::<GameAssets>().unwrap();
+    let text_styles = world.get_resource::<TextStyles>().unwrap();
+
+    if !game_assets.loaded(&asset_server) || !text_styles.loaded(&asset_server) {
+        return;
     }
+    world
+        .resource_mut::<NextState<AppState>>()
+        .set(AppState::InGame);
+    world
+        .resource_mut::<NextState<GameState>>()
+        .set(GameState::Playing);
+}
+
+fn cleanup_main_menu(
+    mut commands: Commands,
+    node: Query<Entity, With<MenuNode>>,
+    camera: Query<Entity, With<MenuCamera>>,
+) {
+    commands.entity(node.single()).despawn_recursive();
+    commands.entity(camera.single()).despawn_recursive();
 }
