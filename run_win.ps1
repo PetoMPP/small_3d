d554cf45dd8dea0b4507c8b50e0b1fa616b4f626
PetoMPP <piotreq22@gmail.com>
2024-05-26 18:54:08 +0200

    add docker for windows

--- run_win.ps1
+++ run_win.ps1
@@ -1,2 +1,2 @@
-cargo build --target=x86_64-pc-windows-msvc --release
-& ".\target\release\small_3d.exe"
+docker run --rm -v "${PWD}:/app" -t small_3d_win cargo build --target=x86_64-pc-windows-msvc
+& ".\target\x86_64-pc-windows-msvc\release\small_3d.exe"
